8cd86eb351ee301f9b207643ddb128b1cef053a3
diorgnes <diodev.@gmail.com>
2025-04-29 17:47:48 -0300

    TiposVariaveis

--- Ttipos-variaveis/src/TiposVariaveis.java
+++ Ttipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+        String nome = "Diórgenes Moura";
+
+        double salarioMinimo = 2500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // Casting necessário para converter int para short
+        
+        int numero = 5;
+
+        numero = 10; // Reatribuição de valor}
+
+        System.err.println(numero);
+
+        final  double VALOR_DE_PI = 3.14; // Constante, não pode ser reatribuído
+
+        System.err.println(nome);
+   }
 }
